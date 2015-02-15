b7e6bd063327d28f3cc4c56c14c186d3c9944f82
daviddlw <davidjefiny@gmail.com>
2015-02-15 23:52:30 +0800

    切换成org.springframework.web.context.ContextLoaderListener的方式 demo

--- src/main/java/com/david/web/TestServlet.java
+++ src/main/java/com/david/web/TestServlet.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import com.david.dto.Person;
 
@@ -25,7 +27,17 @@ public class TestServlet extends HttpServlet
 	{
 		// TODO Auto-generated method stub
 		ServletContext context = getServletContext();
-		ApplicationContext ctx = (ApplicationContext) context.getAttribute("ApplicationContext");
+		
+		/*
+		 * 使用自定义的CustomServletListener时候因为是在相应的attributes里面设置过值的所以可以通过相应属性之获取
+		 */
+//		ApplicationContext ctx = (ApplicationContext) context.getAttribute("ApplicationContext");
+		
+		/*
+		 * 使用spring的自带的listener的话，可以从WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE属性中获取
+		 * 当然也可以直接使用它提供的WebApplicationContextUtils.getWebApplicationContext(context)工具类就行
+		 */
+		ApplicationContext ctx = WebApplicationContextUtils.getWebApplicationContext(context);
 		Person person = ctx.getBean(Person.class);
 		person.hello();		
 	}
